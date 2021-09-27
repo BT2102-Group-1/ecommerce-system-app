e2287a7639ba956744cfa8b3e3341a0010c3e658
hongpeigoh <hong_pei_99@hotmail.com>
2021-09-27 23:08:26 +0800

    Modularising controller

--- controller.py
+++ controller.py
@@ -1,10 +1,21 @@
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 import pandas as pd
 
-db = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
+class Connection:
+    def __init__(self):
+        this.engine = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
+        this.connection = db.connect()
 
-print(db)
+    def init_table(self):
+        init_table_file = open('sql/Database.sql')
+        init_table_query = text(init_table_file.read())
 
-print(pd.read_sql('Customer', db))
-print(pd.read_sql_query('SELECT * FROM Customer WHERE gender = "F"', db))
+        this.connection.execute(init_table_query)
+
+if __name__ == 'main':
+    db = Connection()
+    print(db.engine)
+    print(pd.read_sql('Customer', db.connection))
+    print(pd.read_sql_query('SELECT * FROM Customer WHERE gender = "F"', db))
 
