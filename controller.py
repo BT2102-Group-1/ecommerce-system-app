1924fc6c8d33c1c76c5a8d97af30845b3e453f48
hongpeigoh <hong_pei_99@hotmail.com>
2021-09-27 22:25:09 +0800

    Test

---
+++ controller.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+db = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
+
+print(db)
+
+print(pd.read_sql('Item', db))
+
