e8de12ac936ce1883aa6d07e8762fee0285c6b04
hongpeigoh <hong_pei_99@hotmail.com>
2021-09-28 03:06:12 +0800

    Bugfixes
    Change database name from Capitalcase to lowercase because Windows
    Include use_pure to avoid connector SSL issues

--- controller.py
+++ controller.py
@@ -1,4 +1,3 @@
-from numpy.lib.function_base import append
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 import pandas as pd
@@ -6,15 +5,15 @@ import numpy as np
 
 class Connection:
     def __init__(self):
-        self.engine = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
+        self.engine = create_engine('mysql+mysqlconnector://root:password@localhost/oshes', connect_args={'use_pure': True})
         self.connection = self.engine.connect()
 
 if __name__ == '__main__':
     db = Connection()
     print(db.engine)
-    # print(pd.read_sql('Customer', db.connection))
-    # print(pd.read_sql_query('SELECT * FROM Customer WHERE gender = "F"', db.connection))
+    print(pd.read_sql('customer', db.connection))
+    print(pd.read_sql_query('SELECT * FROM Customer WHERE gender = "F"', db.connection))
 
     insert = pd.DataFrame(np.array([['Lyn Tan', 'F', '123 Tiong Bahru Rd', 'hihi@jeno.com', 'cpassword', '92969405']]), columns=['name', 'gender', 'address', 'email', 'password', 'phoneNo']) 
-    insert.to_sql('Customer', db.connection, if_exists='append', index_label='customerId') # write “customers” to database
-    print(pd.read_sql('Customer', db.connection))
+    insert.to_sql('customer', db.connection, if_exists='append', index_label='customerId') # write “customers” to database
+    print(pd.read_sql('customer', db.connection))
