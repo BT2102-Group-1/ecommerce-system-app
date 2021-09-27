929124d72bb5e4834064befd07a002e0abfbb6ba
hongpeigoh <hong_pei_99@hotmail.com>
2021-09-27 23:09:15 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 class Connection:
     def __init__(self):
-        this.engine = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
-        this.connection = db.connect()
+        self.engine = create_engine('mysql+mysqlconnector://root:password@localhost/oshes')
+        self.connection = db.connect()
 
     def init_table(self):
         init_table_file = open('sql/Database.sql')
         init_table_query = text(init_table_file.read())
 
-        this.connection.execute(init_table_query)
+        self.connection.execute(init_table_query)
 
 if __name__ == 'main':
     db = Connection()
