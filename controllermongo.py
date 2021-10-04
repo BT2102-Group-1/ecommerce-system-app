c6e995c9e79c8b39c7a6d67110cb9e7516db14ca
Lyn <lyntan2001@outlook.com>
2021-10-04 15:51:01 +0800

    Update controllermongo.py

--- controllermongo.py
+++ controllermongo.py
@@ -7,13 +7,43 @@ mongodb = MongoClient('localhost', 27017) # connect to mongoDB at localhost, por
 db = mongodb.oshes # use "oshes" database
 # print(db)
 
-collection = db.Product
+collection = db.Item
 # print(collection)
 
-cursor = collection.find()
+# cursor = collection.find({"PurchaseStatus": "Unsold"}) # find all unsold items
+# r = (list(cursor)) # convert the documents object into a list
+# S = pd.DataFrame(r)
+# del S["_id"]
+# print(S)
+
+
+# Takes in a selection in the form of a dictionary, e.g. { "model": "safe1", "category": "locks",  "color": "white", "productionYear": "2011", "factory": "Singapore", "powerSupply": "Battery"}
+# If user does not select any filter for the category, the value of the category will be an empty string, eg category: ""
+# Returns an array of UNSOLD items
+def customerSearch(selection): 
+    
+    queryDict = dict()
+    for (key, value) in selection.items():
+      if value != "":
+
+        if key == "productionYear":
+          queryDict[key[:1].upper() + key[1:]] = float(value) # Make key title case, no spaces
+        else:
+          queryDict[key[:1].upper() + key[1:]] = value.capitalize() 
+
+
+
+    # ONLY OF UNSOLD!!
+    # return should be like this
+    # arr = [[productNameVar, price, # of items in stock, warranty, [list of items objects]],[[productNameVar, price, # of items in stock, warranty, [list of items objects]],[[productNameVar, price, # of items in stock, warranty, [list of items objects]] etc ...
+    # BECAUSE we follow the design sent into the group drawn by hongpei sent by megan
+    return queryDict
+
+queryDict = customerSearch({ "model": "", "category": "locks",  "color": "white", "productionYear": "2015", "factory": "", "powerSupply": "Battery"})
+queryDict["PurchaseStatus"] = "Unsold"
+print(queryDict)
+cursor = collection.find(queryDict) # find all unsold items
 r = (list(cursor)) # convert the documents object into a list
-print(r)
-
-S = pd.DataFrame(r)
-print(S)
-
+T = pd.DataFrame(r)
+del T["_id"]
+print(T)
