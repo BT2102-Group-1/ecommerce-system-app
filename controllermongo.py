583beab97ac912c13fd68037040b1ad7b76dcc3e
Lyn <lyntan2001@outlook.com>
2021-10-04 16:16:18 +0800

    Update controllermongo.py

--- controllermongo.py
+++ controllermongo.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient # import MongoClient
+from pymongo import MongoClient
+from bson.son import SON
 import pandas as pd
 import numpy as np
 
@@ -10,12 +11,6 @@ db = mongodb.oshes # use "oshes" database
 collection = db.Item
 # print(collection)
 
-# cursor = collection.find({"PurchaseStatus": "Unsold"}) # find all unsold items
-# r = (list(cursor)) # convert the documents object into a list
-# S = pd.DataFrame(r)
-# del S["_id"]
-# print(S)
-
 
 # Takes in a selection in the form of a dictionary, e.g. { "model": "safe1", "category": "locks",  "color": "white", "productionYear": "2011", "factory": "Singapore", "powerSupply": "Battery"}
 # If user does not select any filter for the category, the value of the category will be an empty string, eg category: ""
@@ -23,27 +18,44 @@ collection = db.Item
 def customerSearch(selection): 
     
     queryDict = dict()
+    queryDict["PurchaseStatus"] = "Unsold" # find only Unsold items
     for (key, value) in selection.items():
       if value != "":
 
         if key == "productionYear":
           queryDict[key[:1].upper() + key[1:]] = float(value) # Make key title case, no spaces
         else:
-          queryDict[key[:1].upper() + key[1:]] = value.capitalize() 
+          queryDict[key[:1].upper() + key[1:]] = value[:1].upper() + value[1:] # Make value title case, no spaces (esp for Model)
+
+    print(queryDict) # debugging
+
+    pipeline = [
+      { 
+        "$match": queryDict 
+      },
+      { 
+        "$group": {
+          "_id": "$Model", 
+          "numItemsInStock": {"$sum": 1}, 
+          "itemIDs": {"$push": "$ItemID"}
+        }
+      },
+      {
+        "$sort": SON([("Model", 1)])
+      },
+    ]
+
+    cursor = collection.aggregate(pipeline)
+  
+    results = list(cursor) # convert the documents object into a list
+    df = pd.DataFrame(results)
+    df.rename(columns={'_id': 'productName'}, inplace=True)
+    print(df) # debugging
 
 
-
-    # ONLY OF UNSOLD!!
     # return should be like this
     # arr = [[productNameVar, price, # of items in stock, warranty, [list of items objects]],[[productNameVar, price, # of items in stock, warranty, [list of items objects]],[[productNameVar, price, # of items in stock, warranty, [list of items objects]] etc ...
     # BECAUSE we follow the design sent into the group drawn by hongpei sent by megan
-    return queryDict
+    return df
 
-queryDict = customerSearch({ "model": "", "category": "locks",  "color": "white", "productionYear": "2015", "factory": "", "powerSupply": "Battery"})
-queryDict["PurchaseStatus"] = "Unsold"
-print(queryDict)
-cursor = collection.find(queryDict) # find all unsold items
-r = (list(cursor)) # convert the documents object into a list
-T = pd.DataFrame(r)
-del T["_id"]
-print(T)
+customerSearch({ "model": "", "category": "locks",  "color": "white", "productionYear": "2015", "factory": "", "powerSupply": "Battery"})
