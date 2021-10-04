ff41e5d89d0d1cfcd196795037db92bfc7694d47
Lyn <lyntan2001@outlook.com>
2021-10-04 14:53:35 +0800

    Update customerLogin, registerCustomer, adminLogin, requestService, getUnpaidServiceCustomers

--- controller.py
+++ controller.py
@@ -18,28 +18,42 @@ if __name__ == '__main__':
     # insert.to_sql('customer', db.connection, if_exists='append', index_label='customerId') # write “customers” to database
     # print(pd.read_sql('customer', db.connection))
 
--------------------------------------------------##--Customer Login Page--#------------------------------------------------------
-# LYN
-def customerLogin(email, password):
-  # check against customer database that it exists and matches
-
-  result = pd.read_sql_query('SELECT customerId, password FROM Customer WHERE email = "%s"' % email, db.connection)
-
-  # return generated customerId
-  # -1 means unsuccessful login (customer don't exist)
+#-------------------------------------------------##--Customer Login Page--#------------------------------------------------------
+# Return generated customerId for successful login
+# Return -1 for unsuccessful login (i.e. customer doesn't exist)
+# we will use this customerId as global variable to be used later as our parameter values
+def customerLogin(self, email, password):
   
- # we will use this customerId as global variable to be used later as our parameter values
+  # check against customer database that it exists and matches
+  result = pd.read_sql_query('SELECT customerId FROM Customer WHERE email = "%s" AND password = "%s"' % (email, password), self.connection)
 
--------------------------------------------------##--Customer Registration Page--#-----------------------------------------------
-# LYN
-def registerCustomer(email, password, name, address, phoneNo, gender):
-  # validation checks
-  # - email must not exist in database
+  if (result.empty):
+    return -1
+  
+  return result['customerId'][0]
+
+#-------------------------------------------------##--Customer Registration Page--#-----------------------------------------------
+# Return generated customerId for successful registration
+# Return -1 for unsuccessful registration
+def registerCustomer(self, email, password, name, address, phoneNo, gender):
+  # validation check - email must not exist in database
+  checkIfEmailExists = pd.read_sql_query('SELECT customerId FROM Customer WHERE email = "%s"' % email, self.connection)
+  if (not checkIfEmailExists.empty):
+    return -1
+
   # create customer in database + generate customerId on your end
-  # return generated customerId
-  # -1 means unsuccessful login
+  newCustomer = pd.DataFrame(np.array([[name, gender, address, email, password, phoneNo]]), columns=['name', 'gender', 'address', 'email', 'password', 'phoneNo']) 
+  newCustomer.to_sql('Customer', self.connection, if_exists='append', index_label='customerId') # write “newCustomer” to database
+
+  newCustomerId = pd.read_sql_query('SELECT customerId FROM Customer WHERE email = "%s"' % email, self.connection)
+  
+  # If writing to database unsuccessful, return -1
+  if (newCustomerId.empty):
+    return -1
+  
+  return newCustomerId['customerId'][0]
     
--------------------------------------------------##--Product Search Page--#-----------------------------------------------------
+# -------------------------------------------------##--Product Search Page--#-----------------------------------------------------
 def customerSearch(selection):
   # selection comes in the form of a dictionary
   #{ "model": "safe1", "category": "locks",  "colour": "white", "productionYear": "2011", "factory": "Singapore", "powerSupply": "Battery"}
@@ -71,13 +85,13 @@ def purchase(itemId, customerId, date):
 
   # return true if successful, false otherwise (in any case where there are two customers viewing a product, one bought alr but not enough time to update in the product page and hence, the 2nd customer click yeet)
 
--------------------------------------------------##--Purchase History Page--#-----------------------------------------------
+# -------------------------------------------------##--Purchase History Page--#-----------------------------------------------
 def getPurchaseHistory(customerId):
    # return all products bought by customer in the form of an array, consisting of item objects sorted base on purchase date (latest at the top)
 
   # item object should have itemid, price, model name, category, production year, colour, factory, powersupply, purchaseDate 
 
--------------------------------------------------##--Request Service Page--#-----------------------------------------------
+# -------------------------------------------------##--Request Service Page--#-----------------------------------------------
 def getUnrequestedItems(customerId):
   # return all products bought by customer which DOES NOT HAVE A SERVICE REQUESTED OR HAD A MOST RECENT REQUEST WITH REQUEST STATUS AS CANCELLED OR COMPLETED in the form of an array, consisting of item objects 
 
@@ -89,7 +103,7 @@ def submitRequest(itemId):
   # change request status, generate service if under warranty
   # no need to return anything
 
--------------------------------------------------##--List of Requests Page--#-----------------------------------------------
+# -------------------------------------------------##--List of Requests Page--#-----------------------------------------------
 def retrieveRequests(customerId):
   # return all products with A REQUEST in the form of an array, consisting of requests objects
 
@@ -104,13 +118,19 @@ def onPay(itemId):
 def onCancelRequest(itemId):
   # change request staus to cancelled 
 
--------------------------------------------------##--Admin Login Page--#-----------------------------------------------
-# LYN
-def adminLogin(username, password):
+# -------------------------------------------------##--Admin Login Page--#-----------------------------------------------
+# Returns True if admin name exists and password matches
+# Returns False otherwise (i.e. unsuccessful login)
+def adminLogin(self, adminId, password):
   # check against admin database that it exists and matches
-  # return True
+  result = pd.read_sql_query('SELECT adminId FROM Administrator WHERE adminId = "%s" AND password = "%s"' % (adminId, password), self.connection)
+
+  if (result.empty):
+    return False
+  
+  return True
  
--------------------------------------------------##--Admin Menu Page--#-----------------------------------------------
+# -------------------------------------------------##--Admin Menu Page--#-----------------------------------------------
 
 def initialiseDatabase():
   # button does the populating of the database according to the PDF requirements (UNSOLD and SOLD items for each ProductID)
@@ -124,7 +144,7 @@ def viewInventory():
   
   # for front end side: it will display the table just like the PDF
 
--------------------------------------------------##--Product Catalogue Page --#-----------------------------------------------
+# -------------------------------------------------##--Product Catalogue Page --#-----------------------------------------------
 
 def adminSearch(selection):
   # selection comes in the form of a dictionary
@@ -138,25 +158,25 @@ def adminSearch(selection):
   
   # BECAUSE we follow the design sent into the group drawn by hongpei sent by megan 
 
--------------------------------------------------##--View Service List Page--#-----------------------------------------------
-# LYN
-def requestServices():
-  # display all past service sorted based on service status (awaiting approval on top, in progress in the middle, completed at the bottom), then sorted by service id (decreasing order, largest which is the latest service is on top)
+# -------------------------------------------------##--View Service List Page--#-----------------------------------------------
+# Display all past service sorted based on service status (awaiting approval on top, in progress in the middle, completed at the bottom), 
+# then sorted by service id (decreasing order, largest which is the latest service is on top)
+# Return a dataframe of services which have the columns: serviceid, itemid, customerid, service status
+def requestServices(self):
+    return pd.read_sql_query('''SELECT s.serviceId, r.itemId, r.customerId, s.serviceStatus FROM Service AS s INNER JOIN Request AS r ON s.requestId = r.requestId
+                                    ORDER BY FIELD(s.serviceStatus,'Waiting for approval', 'In progress', 'Completed'), s.serviceId DESC''', self.connection)
 
-  # return in the form of array of service objects which has the attributes: serviceid, itemid, customerid, service status
-  
 def approveServiceRequest(serviceId):
    # clicking approved checkbox will update service status to be in progress.
   
 def completeServiceRequest(serviceId):
    # clicking completed checkbox will update service status to be completed.
 
--------------------------------------------------##--View Requests (Unpaid Service Fee)--#--------------------------------------
-# LYN
-def getUnpaidServiceCustomers():
-  # not including customers whose requests have been cancelled due to non-payment
-
-  # include customers whose request status is "submitted and waiting for payment"
-
-  # return in the form of array of requests objects which has requestId, requestDate, requestStatus, customerId, itemId
+# -------------------------------------------------##--View Requests (Unpaid Service Fee)--#--------------------------------------
+# not including customers whose requests have been cancelled due to non-payment
+# include customers whose request status is "submitted and waiting for payment"
+# Return a dataframe of requests which have columns requestId, requestDate, requestStatus, customerId, itemId
+def getUnpaidServiceCustomers(self):
+  
+  return pd.read_sql_query('SELECT requestId, requestDate, requestStatus, customerId, itemId FROM oshes.Request WHERE requestStatus = "Submitted and Waiting for payment"', self.connection)
   
